0d88f937723abae3e712412423847722eb14256b
4everVortex <119734721+4everVortex@users.noreply.github.com>
2022-12-12 14:14:47 +0700

    Update ThemeManager.lua

--- BlueLinoria/ThemeManager.lua
+++ BlueLinoria/ThemeManager.lua
@@ -73,7 +73,7 @@ local ThemeManager = {} do
 		writefile(self.Folder .. '/themes/default.txt', theme)
 	end
 
-	function ThemeManager:CreateThemeManager(		groupbox)
+		local groupbox = ThemeManager:AddLeftGroupbox('             Themes')
 		groupbox:AddLabel('Background color'):AddColorPicker('BackgroundColor', { Default = self.Library.BackgroundColor });
 		groupbox:AddLabel('Main color')	:AddColorPicker('MainColor', { Default = self.Library.MainColor });
 		groupbox:AddLabel('Accent color'):AddColorPicker('AccentColor', { Default = self.Library.AccentColor });
